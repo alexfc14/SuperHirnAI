c04370047450e33b36e5fdb4a8d4657f8b987f91
Alex Ferrer <alexfc.14@gmail.com>
2022-04-10 23:12:27 +0200

    compute best opener

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
+from collections import Counter
 import pulp
 import itertools
 import numpy as np
 from Alice import evaluate_guess
 from generator import generator
+from itertools import islice
 
 def get_guess(event):
     return event[0]
@@ -39,12 +41,12 @@ def itertools_generator(n_colors, codelength):
     for i in itertools.product(range(n_colors), repeat=codelength):
         yield np.array(i)
 
-def is_compatible(candidate, guess, guess_output):
-    return guess_output == evaluate_guess(candidate, guess, verbose=False)
+def is_compatible(secret, guess, guess_output):
+    return guess_output == evaluate_guess(secret, guess, verbose=False)
 
-def is_compatible_with_history(candidate, history):
+def is_compatible_with_history(secret, history):
     return all(
-        is_compatible(candidate, guess, output)
+        is_compatible(secret, guess, output)
         for guess, output in history#[::-1]
     )
 
@@ -66,43 +68,63 @@ def information(n_codes, n_colors):
     return np.log2(n_codes) / np.log2(n_colors)
 
 
-def get_entropy(guess, n_colors, codelength):
+def get_entropy_gain(guess, n_colors, codelength):
     Nc = n_colors**codelength
     secret_probability = 1/Nc
     prior_information = information(Nc, n_colors)
     entropy = 0
+    codes_by_output = {}
     for secret in generator(n_colors, codelength):
-        Ns = count_survivors(
-            secret,
-            guess,
-            candidates=generator(n_colors, codelength)
-        )
+        output = evaluate_guess(secret, guess, verbose=False)
+        if output not in codes_by_output:
+            codes_by_output[output] = 1
+        else:
+            codes_by_output[output] += 1
+        Ns = codes_by_output[output]
         posterior_information = information(Ns, n_colors)
         information_gain = prior_information - posterior_information
         entropy += secret_probability * information_gain
     return entropy
 
+def is_representer(guess):
+    if guess[0] != 0:
+        return False
+    diff = guess[1:] - guess[:-1]
+    if not all(0 <= diff):
+        return False
+    if not all(diff <= 1):
+        return False
+    
+    # Smaller values should have higher value counts
+    counts = np.array(list(Counter(guess).values()))
+    diff = counts[1:] - counts[:-1]
+    if not all(diff <= 0):
+        return False
+    
+    return True
 
 if __name__ == "__main__":
-    n_colors = 4
-    codelength = 5
+    n_colors = 10
+    codelength = 8
 
     max_entropy = 0
     best_guess = None
 
-    # openers = itertools_generator(n_colors, codelength)
-    openers = [
-        np.array((0, 1, 2, 3, 4)),
-        np.array((0, 0, 1, 2, 3)),
-        np.array((0, 0, 1, 1, 2)),
-        np.array((0, 0, 0, 1, 2)),
-        np.array((0, 0, 0, 1, 1)),
-        np.array((0, 0, 0, 0, 1)),
-        np.array((0, 0, 0, 0, 0))
-    ]
+    # openers = [
+    #     np.array((0, 1, 2, 3, 4)),
+    #     np.array((0, 0, 1, 2, 3)),
+    #     np.array((0, 0, 1, 1, 2)),
+    #     np.array((0, 0, 0, 1, 2)),
+    #     np.array((0, 0, 0, 1, 1)),
+    #     np.array((0, 0, 0, 0, 1)),
+    #     np.array((0, 0, 0, 0, 0))
+    # ]
+    openers = itertools_generator(n_colors, codelength)
 
     for guess in openers:
-        entropy = get_entropy(guess, n_colors, codelength)
+        if not is_representer(guess):
+            continue
+        entropy = get_entropy_gain(guess, n_colors, codelength)
         if entropy > max_entropy:
             max_entropy = entropy
             best_guess = guess
